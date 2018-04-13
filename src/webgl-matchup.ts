8e8809d92d8eda11e7138e44cce41b1867eb733f
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-12 22:14:56 -0700

    Fixup matchups.

--- src/webgl-matchup.ts
+++ src/webgl-matchup.ts
@@ -1,63 +1,145 @@
+import {mat4, vec3} from 'gl-matrix';
 
-export class AtBat {
-  // gl: WebGLRenderingContext;
+import {create, degToRad} from './webgl-core';
+import {Field} from './webgl-field';
+import {Pitch, PitchObj} from './webgl-pitch';
 
-  // pitch: Pitch;
-  // field: Field;
-  // strikezone: StrikeZone;
+export class Matchup {
+  gl: WebGLRenderingContext;
 
-  // pMatrix: mat4;
-  // cMatrix: mat4;
+  pMatrix: mat4;
+  cMatrix: mat4;
 
-  // constructor() {
-  //   this.gl = create('webgl-canvas') as WebGLRenderingContext;
+  // TODO(kreeger): fix me.
+  ballImage: HTMLImageElement;
 
-  //   this.pitch = new Pitch(this.gl);
-  //   this.field = new Field(this.gl);
-  //   this.strikezone = new StrikeZone(this.gl);
+  field: Field;
 
-  //   const ballImage = new Image();
-  //   ballImage.onload = () => {
-  //     this.pitch.setTexture(this.gl, ballImage);
+  // tslint:disable-next-line:no-any
+  data: any;
+  pitches: Pitch[];
 
-  //     this.gl.clearColor(0.25, 0.25, 0.25, 1.0);
-  //     this.gl.enable(this.gl.DEPTH_TEST);
+  pitchIndex: number;
+  animate: boolean;
 
-  //     this.pMatrix = mat4.create();
-  //     this.cMatrix = mat4.create();
+  testRad: number;
 
-  //     // 6ft == 1.8288m
-  //     mat4.identity(this.cMatrix);
-  //     mat4.translate(this.cMatrix, this.cMatrix, [0.0, -1.8288, -4.0]);
+  constructor() {
+    this.gl = create('webgl-canvas') as WebGLRenderingContext;
 
-  //     this.tick();
-  //   };
-  //   ballImage.src = '/images/baseball2.png';
-  // }
+    this.pMatrix = mat4.create();
+    this.cMatrix = mat4.create();
 
-  // tick() {
-  //   // requestAnimFrame(() => { this.tick(); });
-  //   this.drawScene();
-  //   this.animate();
-  // }
+    // Catchers perspective to start with.
+    this.displayCatcher();
 
-  // drawScene() {
-  //   const gl = this.gl;
+    this.field = new Field(this.gl);
+    this.pitches = [];
+    this.pitchIndex = -1;
 
-  //   // TODO(kreeger): Fix this:
-  //   // gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-  //   // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    this.animate = true;
+  }
 
-  //   // mat4.perspective(
-  //   //     this.pMatrix, 45, gl.viewportWidth / gl.viewportHeight, 0.1,
-  //   500.0);
+  async load(): Promise<void> {
+    // TODO(kreeger): Fix this.
+    const ballImage = new HTMLImageElement();
+    ballImage.onload = () => {
+      this.ballImage = ballImage;
+      // TODO - promise return here...
+    };
+    ballImage.src = '/images/baseball2.png';
+  }
 
-  //   mat4.multiply(this.pMatrix, this.pMatrix, this.cMatrix);
+  // tslint:disable-next-line:no-any
+  setData(data: any) {
+    this.data = data;
+    this.pitchIndex = 0;
+    this.pitches = [];
+    if (Array.isArray(data.pitch)) {
+      data.pitch.forEach((pitch: PitchObj) => {
+        this.pitches.push(new Pitch(this.gl, pitch, this.ballImage));
+      });
+    } else {
+      this.pitches.push(new Pitch(this.gl, data.pitch, this.ballImage));
+    }
+  }
 
-  //   // this.field.draw(gl, this.pMatrix);
-  //   this.strikezone.draw(gl, this.pMatrix);
-  //   // this.pitch.draw(gl, this.pMatrix);
-  // }
+  restart() {
+    this.animate = false;
+    this.pitchIndex = 0;
+    for (let i = 0; i < this.pitches.length; i++) {
+      this.pitches[i].restart();
+    }
+    this.animate = true;
+  }
 
-  // animate() { this.pitch.animate(); }
+  displayCatcher() {
+    mat4.identity(this.cMatrix);
+    mat4.translate(this.cMatrix, this.cMatrix, [0.0, -0.85, -2.1]);
+    const axis = vec3.fromValues(1, 0, 0);
+    const radians = degToRad(-90);
+    mat4.rotate(this.cMatrix, this.cMatrix, radians, axis);
+  }
+
+  displayPitcher() {
+    mat4.identity(this.cMatrix);
+    mat4.translate(this.cMatrix, this.cMatrix, [0.0, -1.0, -21.0]);
+
+    const axis = vec3.fromValues(0, 1, 1);
+    this.testRad = 0.5;
+    const radians = degToRad(180);
+    mat4.rotate(this.cMatrix, this.cMatrix, radians, axis);
+  }
+
+  displayBirdsEye() {
+    mat4.identity(this.cMatrix);
+    mat4.translate(this.cMatrix, this.cMatrix, [0.0, -7.4, -18.0]);
+
+    // Adjust angle to look a little better
+    const axis = vec3.fromValues(1, 0, 0);
+    const radians = degToRad(-20);
+    mat4.rotate(this.cMatrix, this.cMatrix, radians, axis);
+  }
+
+  tick() {
+    window.requestAnimationFrame(() => { this.tick(); });
+
+    if (this.animate) {
+      if (this.pitchIndex < this.pitches.length) {
+        // Display pitch at index.
+        this.pitches[this.pitchIndex].animate();
+        this.pitches[this.pitchIndex].showStrikeZone = true;
+
+        if (this.pitches[this.pitchIndex].isDone) {
+          this.pitches[this.pitchIndex].showStrikeZone = false;
+          this.pitchIndex++;
+          if (this.pitchIndex < this.pitches.length) {
+            this.pitches[this.pitchIndex].showStrikeZone = true;
+          }
+        }
+      }
+    }
+
+    this.drawScene();
+  }
+
+  drawScene() {
+    const gl = this.gl;
+    this.gl.clearColor(0.25, 0.25, 0.25, 1.0);
+
+    // TODO(kreeger): Fix this.
+    // gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // TODO(kreeger): Fix this.
+    // mat4.perspective(
+    //     this.pMatrix, degToRad(45), gl.viewportWidth / gl.viewportHeight,
+    //     0.1, 500.0);
+    mat4.multiply(this.pMatrix, this.pMatrix, this.cMatrix);
+
+    this.field.draw(gl, this.pMatrix);
+    for (let i = 0; i < this.pitches.length; i++) {
+      this.pitches[i].draw(gl, this.pMatrix);
+    }
+  }
 }
