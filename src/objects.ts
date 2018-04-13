26b291e6979ad8e85089584c5245662a92c5e426
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-12 20:53:02 -0700

    Initial commit.

---
+++ src/objects.ts
@@ -0,0 +1,303 @@
+class Polygon {
+  vertices: number[];
+  colors: number[];
+  vertexPositionBuffer: WebGLBuffer;
+  vertexColorBuffer: WebGLBuffer;
+
+  constructor(gl: WebGLRenderingContext) {
+    this.vertices = [];
+    this.colors = [];
+    this.vertexPositionBuffer = gl.createBuffer();
+    this.vertexColorBuffer = gl.createBuffer();
+  }
+
+  setBuffers(gl: WebGLRenderingContext) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexColorBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER, new Float32Array(this.colors), gl.STATIC_DRAW);
+  }
+
+  bindVerticesBuffer(gl: WebGLRenderingContext) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+  }
+
+  bindColorBuffer(gl: WebGLRenderingContext) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexColorBuffer);
+  }
+
+  getScaleSize() { return Math.sin(45 / 180 * Math.PI) * this.getHeight(); }
+
+  getVerticesSize() { return 3; }
+
+  getColorSize() { return 4; }
+
+  getHeight() { return 0; }
+
+  getVerticesCount() { return this.vertices.length / this.getVerticesSize(); }
+
+  getColorCount() { return this.colors.length / this.getColorSize(); }
+}
+
+class Diamond extends Polygon {
+  constructor(gl: WebGLRenderingContext) {
+    super(gl);
+
+    this.vertices = [
+      1.0, 0.0, 0.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0, -1.0,
+      0.0, 1.0, 0.0, 0.0
+    ];
+  }
+}
+
+class Infield extends Diamond {
+  constructor(gl: WebGLRenderingContext) {
+    super(gl);
+
+    this.colors = [
+      0.0, 0.5, 0.0, 1.0, 0.0, 0.7, 0.0, 1.0, 0.0, 0.5, 0.0, 1.0,
+      0.0, 0.5, 0.0, 1.0, 0.0, 0.7, 0.0, 1.0, 0.0, 0.5, 0.0, 1.0
+    ];
+
+    this.setBuffers(gl);
+  }
+
+  getHeight() { return 27.5; }
+}
+
+class Outfield extends Polygon {
+  constructor(gl: WebGLRenderingContext) {
+    super(gl);
+
+    this.vertices = [
+      0.0, -0.5, 0.0, -0.575, 0.075, 0.0, -0.347, 0.347, 0.0,
+      0.0, -0.5, 0.0, -0.347, 0.347, 0.0, 0.0,    0.5,   0.0,
+      0.0, -0.5, 0.0, 0.0,    0.5,   0.0, 0.347,  0.347, 0.0,
+      0.0, -0.5, 0.0, 0.347,  0.347, 0.0, 0.575,  0.075, 0.0
+    ];
+
+    this.colors = [];
+    for (let i = 0; i < this.getVerticesCount(); i++) {
+      this.colors.push(0.0);
+      this.colors.push(0.3);
+      this.colors.push(0.0);
+      this.colors.push(1.0);
+    }
+
+    this.setBuffers(gl);
+  }
+
+  getHeight() { return 121.92; }
+}
+
+class Base extends Diamond {
+  constructor(gl: WebGLRenderingContext) {
+    super(gl);
+
+    this.colors = [
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0
+    ];
+
+    this.setBuffers(gl);
+  }
+
+  getHeight() { return 0.38; }
+}
+
+class HomePlate extends Polygon {
+  constructor(gl: WebGLRenderingContext) {
+    super(gl);
+
+    this.vertices = [
+      0.216,  0.216, 0.0, 0.216,  0.0, 0.0, -0.216, 0.216,  0.0,
+      -0.216, 0.216, 0.0, -0.216, 0.0, 0.0, 0.216,  0.0,    0.0,
+      0.216,  0.0,   0.0, -0.216, 0.0, 0.0, 0.0,    -0.216, 0.0
+    ];
+
+    this.colors = [];
+    for (let i = 0; i < this.getVerticesCount(); i++) {
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+    }
+
+    this.setBuffers(gl);
+  }
+
+  getHeight() { return 0.432; }
+
+  getScaleSize() { return 1.0; }
+}
+
+class PitchingRubber extends Polygon {
+  constructor(gl: WebGLRenderingContext) {
+    super(gl);
+
+    this.vertices = [
+      -0.305, 0.076, 0.0, 0.305, 0.076, 0.0, -0.305, -0.076, 0.0, -0.305,
+      -0.076, 0.0, 0.305, 0.076, 0.0, 0.305, -0.076, 0.0
+    ];
+
+    this.colors = [];
+    for (let i = 0; i < this.getVerticesCount(); i++) {
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+    }
+
+    this.setBuffers(gl);
+  }
+
+  getHeight() { return 0.152; }
+
+  getScaleSize() { return 1.0; }
+}
+
+class StrikeZone extends Polygon {
+  constructor(gl: WebGLRenderingContext, bottom: number, top: number) {
+    super(gl);
+
+    const x = 0.216;
+    const y = 0.432;
+
+    this.vertices = [
+      -x, y, top, x, y, top, -x, y, bottom, -x, y, bottom, x, y, bottom, x, y,
+      top
+    ];
+
+    this.colors = [];
+    for (let i = 0; i < this.getVerticesCount(); i++) {
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+      this.colors.push(1.0);
+      this.colors.push(0.25);
+    }
+
+    this.setBuffers(gl);
+  }
+
+  getScaleSize() { return 1.0; }
+}
+
+class Baseball {
+  indexDataSize: number;
+  vertexPositionBuffer: WebGLBuffer;
+  vertexNormalBuffer: WebGLBuffer;
+  vertexTextureCoordBuffer: WebGLBuffer;
+  vertexIndexBuffer: WebGLBuffer;
+
+  constructor(gl: WebGLRenderingContext) {
+    const LATITUDE_BANDS = 30;
+    const LONGITUDE_BANDS = 30;
+
+    // 1.43 in
+    const RADIUS = 0.0363;
+
+    const vertexPositionData = [];
+    const normalData = [];
+    const textureCoordData = [];
+
+    // const NORMAL_DATA_SIZE = 3;
+    // const TEXTURE_COORD_DATA_SIZE = 2;
+    // const VERTEX_POSITION_DATA_SIZE = 3;
+    // const INDEX_DATA_SIZE = 1;
+
+    for (let latNum = 0; latNum <= LATITUDE_BANDS; latNum++) {
+      const theta = latNum * Math.PI / LATITUDE_BANDS;
+      const sinTheta = Math.sin(theta);
+      const cosTheta = Math.cos(theta);
+
+      for (let longNum = 0; longNum <= LONGITUDE_BANDS; longNum++) {
+        const phi = longNum * 2 * Math.PI / LONGITUDE_BANDS;
+        const sinPhi = Math.sin(phi);
+        const cosPhi = Math.cos(phi);
+
+        const x = cosPhi * sinTheta;
+        const y = cosTheta;
+        const z = sinPhi * sinTheta;
+        const u = 1 - (longNum / LONGITUDE_BANDS);
+        const v = 1 - (latNum / LATITUDE_BANDS);
+
+        normalData.push(x);
+        normalData.push(y);
+        normalData.push(z);
+
+        textureCoordData.push(u);
+        textureCoordData.push(v);
+
+        vertexPositionData.push(RADIUS * x);
+        vertexPositionData.push(RADIUS * y);
+        vertexPositionData.push(RADIUS * z);
+      }
+    }
+
+    const indexData = [];
+    for (let latNum = 0; latNum < LATITUDE_BANDS; latNum++) {
+      for (let longNum = 0; longNum < LONGITUDE_BANDS; longNum++) {
+        const first = (latNum * (LONGITUDE_BANDS + 1)) + longNum;
+        const second = first + LONGITUDE_BANDS + 1;
+
+        indexData.push(first);
+        indexData.push(second);
+        indexData.push(first + 1);
+
+        indexData.push(second);
+        indexData.push(second + 1);
+        indexData.push(first + 1);
+      }
+    }
+
+    this.indexDataSize = indexData.length;
+
+    // Create and set buffers.
+    this.vertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER, new Float32Array(vertexPositionData), gl.STATIC_DRAW);
+
+    this.vertexNormalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexNormalBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER, new Float32Array(normalData), gl.STATIC_DRAW);
+
+    this.vertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexTextureCoordBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER, new Float32Array(textureCoordData), gl.STATIC_DRAW);
+
+    this.vertexIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+    gl.bufferData(
+        gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STATIC_DRAW);
+  }
+
+  bindVertexNormalBuffer(gl: WebGLRenderingContext) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexNormalBuffer);
+  }
+
+  bindVertexTextureCoordBuffer(gl: WebGLRenderingContext) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexTextureCoordBuffer);
+  }
+
+  bindVertexPositionBuffer(gl: WebGLRenderingContext) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+  }
+
+  bindVertexIndexBuffer(gl: WebGLRenderingContext) {
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+  }
+
+  getVertexPositionSize() { return 3; }
+
+  getTextureCoordDataSize() { return 2; }
+
+  getNormalDataSize() { return 3; }
+
+  getIndexDataSize() { return this.indexDataSize; }
+}
---
+++ src/webgl-core.ts
@@ -0,0 +1,111 @@
+
+export function create(elementId: string) {
+  const element = document.getElementById(elementId) as HTMLCanvasElement;
+  if (!element) {
+    throw new Error('Could not find root element');
+  }
+
+  const rect = (element.parentNode as HTMLElement).getBoundingClientRect();
+  element.width = rect.width;
+  element.height = rect.width * 0.75;
+
+  const gl = create3DContext(element);
+
+  // TODO(kreeger)
+  //   gl.viewportWidth = element.width;
+  //   gl.viewportHeight = element.width;
+
+  return gl;
+}
+
+export function createShaderProgramFromScripts(
+    gl: WebGLRenderingContext, shaderScriptIds: string[]): WebGLProgram {
+  const shaders = [];
+  for (let i = 0; i < shaderScriptIds.length; i++) {
+    shaders.push(createShaderFromScript(gl, shaderScriptIds[i]));
+  }
+
+  const shaderProgram = gl.createProgram();
+  for (let i = 0; i < shaders.length; i++) {
+    gl.attachShader(shaderProgram, shaders[i]);
+  }
+
+  gl.linkProgram(shaderProgram);
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    throw new Error('Could not init shaders!');
+  }
+
+  return shaderProgram;
+}
+
+export function createShaderProgram(
+    gl: WebGLRenderingContext, fragmentShaderSrc: string,
+    vertexShaderSrc: string) {
+  const fragmentShader =
+      createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSrc);
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSrc);
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    throw new Error('Could not init shaders!');
+  }
+
+  return shaderProgram;
+}
+
+export function degToRad(degrees: number): number {
+  return degrees * Math.PI / 180;
+}
+
+function create3DContext(canvas: HTMLCanvasElement): CanvasRenderingContext2D|
+    WebGLRenderingContext {
+  const names = ['webgl', 'experimental-webgl', 'webkit-3d', 'moz-webgl'];
+  let context = null;
+  for (let i = 0; i < names.length; i++) {
+    try {
+      context = canvas.getContext(names[i]);
+    } catch (e) {
+    }
+    if (context) {
+      break;
+    }
+  }
+  return context;
+}
+
+export function createShader(
+    gl: WebGLRenderingContext, type: number, shaderSrc: string): WebGLShader {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, shaderSrc);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw gl.getShaderInfoLog(shader);
+  }
+  return shader;
+}
+
+export function createShaderFromScript(
+    gl: WebGLRenderingContext, scriptId: string): WebGLShader {
+  let shaderSource = '';
+  let shaderType;
+  const shaderScript = document.getElementById(scriptId) as HTMLScriptElement;
+  if (!shaderScript) {
+    throw new Error('*** Error: unknown script element' + scriptId);
+  }
+  shaderSource = shaderScript.text;
+
+  if (shaderScript.type === 'x-shader/x-vertex') {
+    shaderType = gl.VERTEX_SHADER;
+  } else if (shaderScript.type === 'x-shader/x-fragment') {
+    shaderType = gl.FRAGMENT_SHADER;
+  } else if (
+      shaderType !== gl.VERTEX_SHADER && shaderType !== gl.FRAGMENT_SHADER) {
+    throw new Error('*** Error: unknown shader type');
+  }
+
+  return createShader(gl, shaderType, shaderSource);
+}
